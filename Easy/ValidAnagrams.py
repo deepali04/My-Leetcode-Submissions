679b4b2cfaf59a4f7977575a839c15e424eb54fb
Deepali Nagwade <nagwade.deepali@gmail.com>
2023-01-21 14:28:43 -0500

    Added Valid Anagrams

---
+++ Easy/ValidAnagrams.py
@@ -0,0 +1,8 @@
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    res1 = ''.join(sorted(s))
+    res2 = ''.join(sorted(t))
+    if(res1==res2):
+      return True
+    else:
+      return False
