6d09c7be4ab257cb5047c54bc551911e40b2a737
Deepali Nagwade <nagwade.deepali@gmail.com>
2023-01-21 14:25:48 -0500

    Delete ..Easy directory

--- ..Easy/ValidAnagrams.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        res1 = ''.join(sorted(s))
-        res2 = ''.join(sorted(t))
-        if(res1==res2):
-            return True
-        else:
-            return False
